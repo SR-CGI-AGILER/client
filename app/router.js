b8954667e5b76ee5760bd588b4874d0981c2097f
priyanka <priyankadutta67@gmail.com>
2018-10-09 14:45:50 +0530

    new planner page added

--- app/router.js
+++ app/router.js
@@ -16,6 +16,7 @@ Router.map(function() {
     this.route('myactivity');
     this.route('feeds');
     this.route('new-plan');
+    this.route('planner');
   });
   this.route('start-discussion');
   this.route('tasks-list')
