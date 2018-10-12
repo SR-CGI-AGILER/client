5b7cbd9f86e8ab9879064f5c30682e20c1cf9fb2
Ankit-Mund <ankit7mund@gmail.com>
2018-10-12 20:11:19 +0530

    One-to-One chat implemented

--- app/controllers/activity/planner.js
+++ app/controllers/activity/planner.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-<<<<<<< HEAD
   newCard: [],
 
   actions: {
@@ -10,23 +9,6 @@ export default Controller.extend({
         let newProject = {
             id: Math.random(Math.floor(100000)),
           projectName: this.getProperties('name2').name2
-=======
-    newCard: [
-            
-        ],
-    
-    actions: {
-        addCard() {
-            if(this.getProperties('name2').name2){
-                let newProject = {
-                    projectName: this.getProperties('name2').name2
-                }
-                this.newCard.pushObject(newProject);
-            }            
-        },
-        deleteCard(topping) {
-            this.newCard.removeObject(topping);
->>>>>>> c35f69aa0d9780e2aed7569d7a9c09e2cee03c8c
         }
         this.newCard.pushObject(newProject);
         let createActivity = this.store.createRecord('activity', {
@@ -38,7 +20,7 @@ export default Controller.extend({
       }
     },
     deleteCard(topping) {
-        console.log(topping)
+        // console.log(topping)
         this.store.findRecord('activity', topping.id).then(data => {
             data.set('archiveProject',"true")
             this.newCard.removeObject(topping);
