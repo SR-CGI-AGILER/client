455536414fc18b534b53cb9cd64b8221d370c6e1
priyanka <priyankadutta67@gmail.com>
2018-10-12 09:24:20 +0530

    backend connected

---
+++ app/adapters/activity.js
@@ -0,0 +1,46 @@
+import DS from 'ember-data';
+import Em from 'ember';
+
+export default DS.RESTAdapter.extend({
+    buildURL(){
+        return `http://localhost:8000/api/v1/project`
+    },
+    createRecord(store, type, snapshot) {
+        let data = this.serialize(snapshot);
+
+        return new Promise((resolve) => {
+            Em.$.ajax({
+                async: true,
+                crossDomain: true,
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                url: `http://localhost:8000/api/v1/project`,
+                success: {
+                    200: ()=>{
+                        Em.run(null, resolve);
+                    }
+                }
+            })
+        })
+    },
+    updateRecord(store, type, snapshot) {
+        let data = this.serialize(snapshot);
+
+        return new Promise((resolve) => {
+            Em.$.ajax({
+                async: true,
+                crossDomain: true,
+                type: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                url: `http://localhost:8000/api/v1/project/:projectName`,
+                success: {
+                    200: ()=>{
+                        Em.run(null, resolve);
+                    }
+                }
+            })
+        })
+    }
+});
