0b245efc0850e8a973255c15364c38457b3e3965
Ankit-Mund <ankit7mund@gmail.com>
2018-10-13 17:32:19 +0530

    Room created

--- app/controllers/tasks-list.js
+++ app/controllers/tasks-list.js
@@ -1,7 +1,17 @@
 import Controller from '@ember/controller';
+import Ember from "ember";
+
+// import { io } from "../components/discuss-chat.js";
 
 export default Controller.extend({
-    actions: {
-        
+  actions: {
+    joinButtonPressed() {
+      Ember.$.ajax({
+        type: "POST",
+        url: `http://localhost:3000/chat-room/myroom`
+      });
+      // alert('triggered');
+      this.transitionToRoute('discuss');
     }
+  }
 });
